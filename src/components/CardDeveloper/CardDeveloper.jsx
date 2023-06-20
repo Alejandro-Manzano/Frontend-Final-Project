9ff2fbd1b62eac9464436b1525764a93bf39ac16
Cmrdevelopment <cmrbolsadevelopment@gmail.com>
2023-06-20 17:47:38 +0100

    CardDeveloper jsx y css

---
+++ src/components/CardDeveloper/CardDeveloper.jsx
@@ -0,0 +1,29 @@
+// import ToggleButton from '../Toggle/Toggle';
+import './CardDeveloper.css';
+import { useNavigate } from 'react-router-dom';
+
+export const CardDeveloper = ({ developer }) => {
+  const navigate = useNavigate();
+  // const pathById = `/developer`;
+
+  // console.log("mobileDev: ", mobileDev);
+  return (
+    <section className="developer-Info">
+      <img src={developer.image} alt={`developer's ${developer.name.surname} pic`} />
+      <div className="developer-Info-tabla-name">
+        {developer.name} {developer.surname}
+        <p className="developer-Info-Ubicado"> ✨ Ubicado/a en {developer.city}</p>
+      </div>
+      <h3 className="developer-Info-Rol">{developer.rol}</h3>
+      <p className="developer-Info-ratingsByOthers">
+        {' '}
+        ⭐⭐⭐⭐⭐ {developer.ratingsByOthers} (9)
+      </p>
+      <div className="developer-Info-grupo-technologies">
+        <h4 className="developer-Info-technologies">{developer.technologies}</h4>
+      </div>
+    </section>
+  );
+};
+
+export default CardDeveloper;
