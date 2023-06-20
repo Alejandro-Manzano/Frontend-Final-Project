ba6ccca00ca45628935dd8a8c92aa786c26918cc
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2023-06-20 22:49:09 +0200

    home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,49 @@
 import './Footer.css';
 
-import { useMediaQuery } from 'react-responsive';
-
 const Footer = () => {
-  const isLargeScreen = useMediaQuery({ minWidth: 530 });
+  
   return (
-    <>
-      <div className="whiteContainer"></div>
-      <footer>
-        {isLargeScreen ? (
-          <h4>Powered by cutting-edge technology and 💖 for innovation</h4>
-        ) : (
-          <h4> Passion 💖 for innovation</h4>
-        )}
-        <h4 className="footerText"> Champion Group ✌</h4>
-      </footer>
-    </>
+    
+    <footer>
+
+      <div className='footer-information-container'>
+
+        <div className='footer-infotmation'>
+          <p className='footer-infotmation-title'>Nos encantaría que te unieras a nosotros</p>
+          <p className='footer-information-description'>No te convertiste en desarrollador independiente para pasar horas cada semana <br></br>
+            frente a tu ordenador desplazándote a través de interminables listas de trabajos</p>
+        </div>
+
+        <div className='footer-container-img'>
+          <img src='https://res.cloudinary.com/dhlr7fvd8/image/upload/v1687191076/ProjectFinalBOOTCAMP/LogoWeb/logoWEBfinalProject_qvzlbq.png' alt="icon" className='footer-icon'></img>
+          <p className='footer-icon-p'>Code</p>
+        </div>
+
+      </div>
+
+      <div className='footer-container-form'>
+
+        <p>Contacta</p>
+
+        <form className='footer-form' action="mailto:teammates.neoland23@gmail.com" method="post">
+
+        <label for="name">Nombre:
+        <input className='footer-input-text' type="text" id="name" name="name" required/></label>
+      
+        <label for="mail">Correo electrónico:
+        <input className='footer-input-text' type="email" id="mail" name="mail" required/></label>
+
+        <label for="message">Mensaje:
+        <input className='footer-input-text' id="message" name="message"  required/></label>
+
+        <input className='footer-input-submit' type="submit" value="Contactar"/>
+
+        </form>
+
+      </div>
+      
+    </footer>
+
   );
 };
 
