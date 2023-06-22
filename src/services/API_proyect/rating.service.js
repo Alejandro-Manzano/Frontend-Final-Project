85387944eb3611a3bb461014303893890508f48e
aaitorgutierrezç <gutierrezizquierdoaitor@gmail.com>
2023-06-22 12:36:06 +0200

    comment functionality

--- src/services/API_proyect/rating.service.js
+++ src/services/API_proyect/rating.service.js
@@ -2,7 +2,6 @@ import { updateToken } from '../../util/updateToken';
 import { API } from './service.config';
 
 export const createRating = async (data) => {
-<<<<<<< HEAD
   return API.post('/rating', data, {
     headers: {
       Authorization: `Bearer ${updateToken()}`,
@@ -12,23 +11,10 @@ export const createRating = async (data) => {
     .catch((error) => {
       return error;
     });
-=======
-    return API.post('/rating', data, {
-        headers: {
-            Authorization: `Bearer ${updateToken()}`
-        },
-    })
-        .then((res) => res)
-        .catch((error) => {
-            return error;
-        });
->>>>>>> 68fd69cc3aa5bce8646f65bb2c2f5a5247021a5b
 };
 
 export const updateRating = async (data, id) => {
-<<<<<<< HEAD
   console.log('Front -> createRating -> data: ', data);
-
   return API.patch(`/rating/${id}`, data, {
     headers: {
       Authorization: `Bearer ${updateToken()}`,
@@ -38,17 +24,6 @@ export const updateRating = async (data, id) => {
     .catch((error) => {
       return error;
     });
-=======
-    return API.patch(`/rating/${id}`, data, {
-        headers: {
-            Authorization: `Bearer ${updateToken()}`
-        },
-    })
-        .then((res) => res)
-        .catch((error) => {
-            return error;
-        });
->>>>>>> 68fd69cc3aa5bce8646f65bb2c2f5a5247021a5b
 };
 
 export const getByReference = async (type, idUserWithRating) => {
