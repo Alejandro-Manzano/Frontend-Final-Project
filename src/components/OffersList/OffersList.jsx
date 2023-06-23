5a81f5f729a2ef01d12627a5ea13215f304e4113
Igor <iluzarraga@gmail.com>
2023-06-23 20:22:24 +0200

    Added filters to Offer page

--- src/components/OffersList/OffersList.jsx
+++ src/components/OffersList/OffersList.jsx
@@ -4,8 +4,18 @@ import ReactPaginate from 'react-paginate';
 import { offer_getAll } from "../../services/API_proyect/offer.service"
 import "./OffersList.css";
 import { Spinner } from "../Spinner/Spinner";
-import { sortOfersByAverageScore_descendingOrder } from "../../util/filters/offer.filter";
+import {
+    filterOffersByJobType,
+    filterOffersByLessOrEqualThanAnnualSalary,
+    filterOffersByGreaterOrEqualThanAnnualSalary,
+    filterOffersByOfferState,
+    sortOfersByAverageScore_descendingOrder,
+    sortOffersByAverageScore_ascendingOrder,
+    filterOffersByGreaterOrEqualThanExperienceYears,
+    filterOffersByLessOrEqualThanExperienceYears
+} from "../../util/filters/offer.filter";
 import CardOffer from "../CardOffer/CardOffer";
+import { filterOffersByOfferType } from "../../util/filters/offer.filter";
 
 const OffersList = ({ itemsPerPage }) => {
     const [dataDevelopersList, setDataDevelopersList] = useState([]);
@@ -16,18 +26,288 @@ const OffersList = ({ itemsPerPage }) => {
     const [pageCount, setPageCount] = useState(0);
     const [itemPerPage, setItemPerPage] = useState([])
 
+    // const FilterOnOffType = {
+    //     FilterOn: "FilterOn",
+    //     FilterOff: "FilterOff"
+    // }
+
+    const OfferType = {
+        CompanyOffer: "CompanyOffer",
+        FreelandOffer: "FreelandOffer",
+        FilterOff: "FilterOff"
+    }
+
+    const OfferState = {
+        Close: "Close",
+        Suspended: "Suspended",
+        Open: "Open",
+        FilterOff: "FilterOff"
+    }
+
+    const JobType = {
+        Remote: "Remote",
+        Office: "Office",
+        Hybrid: "Hybrid",
+        FilterOff: "FilterOff"
+    }
+
+    const AverageScoreType = {
+        DescendingOrder: "DescendingOrder",
+        AscendingOrder: "AscendingOrder",
+        FilterOff: "FilterOff"
+    }
+
+    const AnnualSalaryType = {
+        GreaterOrEqualThanAnnualSalary: "GreaterOrEqualThanAnnualSalary",
+        LessOrEqualThanAnnualSalary: "LessOrEqualThanAnnualSalary",
+        FilterOff: "FilterOff"
+    }
+
+    const ExperienceYearsType = {
+        GreaterOrEqualThanExperienceYears: "GreaterOrEqualThanExperienceYears",
+        LessOrEqualThanExperienceYears: "LessOrEqualThanExperienceYears",
+        FilterOff: "FilterOff"
+    }
+
+    const initFiltersType = {
+        ByOfferType: OfferType,
+        ByAverageScore: AverageScoreType,
+    }
+
+    const initFiltersToApply_AllFilterOff = {
+        byExperienceYears: ExperienceYearsType.FilterOff,
+        byAnnualSalary: AnnualSalaryType.FilterOff,
+        byOfferState: OfferState.FilterOff,
+        byJobType: JobType.FilterOff,
+        byOfferType: OfferType.FilterOff,
+        byAverageScore: AverageScoreType.FilterOff,
+    }
+
+    const initFiltersToApply = {
+        // GreaterOrEqualThanExperienceYears, LessOrEqualThanExperienceYears
+        byExperienceYears: ExperienceYearsType.FilterOff,
+        experinceYears: 5,
+
+        // GreaterOrEqualThanAnnualSalary, LessOrEqualThanAnnualSalary
+        byAnnualSalary: AnnualSalaryType.FilterOff,
+        annualSalary: 100,
+
+        // Close, Suspended, Open
+        byOfferState: OfferState.FilterOff,
+
+        // Remote, Office, Hybrid
+        byJobType: JobType.FilterOff,
+
+        // CompanyOffer, FreelandOffer
+        byOfferType: OfferType.FilterOff,
+
+        // DescendingOrder, AscendingOrder
+        byAverageScore: AverageScoreType.DescendingOrder,
+    }
+
+    const [filtersToApply, setFilterToApply] = useState(initFiltersToApply)
+
     const getOffersData = async () => {
         setDownloading(true);
         const dataOffer = await offer_getAll()
 
-        // Filter offers by average score
-        const dataSortByAverageScore = sortOfersByAverageScore_descendingOrder(dataOffer)
+        ////////////////////////////////////////
+        // NOTE: This is for tests. 
+        //       This filters are not gonna be here!
+        //////////////////////////////////////////
 
-        const dataFilterZero = dataSortByAverageScore.slice(0, itemsPerPage)
-        const numerberPage = dataSortByAverageScore.length / itemsPerPage
+        console.log("dataOffer: ", dataOffer)
+
+        let dataFilterByOfferType = null
+        let dataToRender = dataOffer
+
+        //applyFiltersToData(dataOffer)
+        Object.keys(filtersToApply)
+            .filter((filter) => filtersToApply[filter] !== "FilterOff")
+            .map(filterType => {
+                // console.log("map -> Filter type: ", filterType)
+                // console.log("map -> Filter By: ", filtersToApply[filterType])
+
+
+
+                if (filterType === 'byOfferType') {
+                    //  --------------- Filter offers by type ----------
+                    //  ---------  Company or Freeland  ----------------
+
+                    //filterByOfferType(initFiltersToApply[filterType]);
+
+                    //dataFilterByOfferType =
+                    dataToRender =
+                        filterOffersByOfferType(
+                            dataToRender,
+                            //initFiltersToApply[filterType]
+                            filtersToApply[filtersToApply]
+                            //"FreelandOffer" 
+                            //"LookingForJob"
+                        )
+                } else if (filterType === 'byAverageScore') {
+                    // ----- Filter offers by average score ------
+
+                    //switch (initFiltersToApply[filterType]) {
+                    switch (filtersToApply[filterType]) {
+
+                        case "DescendingOrder":
+                            console.log("descencing order")
+                            //let dataFilterDes = dataFilterByOfferType ? dataFilterByOfferType : dataOffer
+                            dataToRender =
+                                sortOfersByAverageScore_descendingOrder(dataToRender)
+                            break
+
+                        case "AscendingOrder":
+                            console.log("ascencing order")
+                            //let dataFilterAsc = dataFilterByOfferType ? dataFilterByOfferType : dataOffer
+                            dataToRender =
+                                sortOffersByAverageScore_ascendingOrder(dataToRender)
+                            break
+
+                        default:
+                            console.log("Error: this should be a new case for byAverageScore")
+                    }
+                } else if (filterType === 'byJobType') {
+                    // ----- Filter offers by Job Type  (Remote, Office, Hybrid) ------
+
+                    //switch (initFiltersToApply[filterType]) {
+                    switch (filtersToApply[filterType]) {
+                        case "Remote":
+                            console.log("JobType: Remote")
+
+                            dataToRender =
+                                filterOffersByJobType(
+                                    dataToRender,
+                                    //initFiltersToApply[filterType]
+                                    filtersToApply[filtersToApply]
+                                )
+                            break
+
+                        case "Office":
+                            console.log("JobType: Office")
+
+                            dataToRender =
+                                filterOffersByJobType(
+                                    dataToRender,
+                                    //initFiltersToApply[filterType]
+                                    filtersToApply[filterType]
+                                )
+                            break
+
+                        case "Hybrid":
+                            console.log("JobType: Hybrid")
+
+                            dataToRender =
+                                filterOffersByJobType(
+                                    dataToRender,
+                                    //initFiltersToApply[filterType]
+                                    filtersToApply[filterType]
+                                )
+                            break
+
+                        default:
+                            console.log("Error: this should be a new case for byJobType")
+                    }
+                } else if (filterType === 'byOfferState') {
+                    dataToRender =
+                        filterOffersByOfferState(
+                            dataToRender,
+                            //initFiltersToApply[filterType]
+                            filtersToApply[filterType]
+                            //"Close" 
+                            //"Suspended"
+                            //"Open"
+                        )
+                } else if (filterType === 'byAnnualSalary') {
+                    // ----- Filter offers by annual salary ------
+
+                    //switch (initFiltersToApply[filterType]) {
+                    switch (filtersToApply[filterType]) {
+
+                        case "GreaterOrEqualThanAnnualSalary":
+                            console.log("GreaterThanAnnualSalary")
+
+                            dataToRender =
+                                filterOffersByGreaterOrEqualThanAnnualSalary(dataToRender, filtersToApply.annualSalary)
+                            break
+
+                        case "LessOrEqualThanAnnualSalary":
+                            console.log("LessThanAnnualSalary")
+
+                            dataToRender =
+                                filterOffersByLessOrEqualThanAnnualSalary(dataToRender, filtersToApply.annualSalary)
+                            break
+
+                        default:
+                            console.log("Error: this should be a new case for byAnnualSalary")
+                    }
+                } else if (filterType === 'byAnnualSalary') {
+                    // ----- Filter offers by annual salary ------
+
+                    //switch (initFiltersToApply[filterType]) {
+                    switch (filtersToApply[filterType]) {
+
+                        case "GreaterOrEqualThanAnnualSalary":
+                            console.log("GreaterOrEqualThanAnnualSalary")
+
+                            dataToRender =
+                                filterOffersByGreaterOrEqualThanAnnualSalary(dataToRender, filtersToApply.annualSalary)
+                            break
+
+                        case "LessOrEqualThanAnnualSalary":
+                            console.log("LessOrEqualThanAnnualSalary")
+
+                            dataToRender =
+                                filterOffersByLessOrEqualThanAnnualSalary(dataToRender, filtersToApply.annualSalary)
+                            break
+
+                        default:
+                            console.log("Error: this should be a new case for byAnnualSalary")
+                    }
+                } else if (filterType === 'byExperienceYears') {
+                    // ----- Filter offers by years of experice  ------
+
+                    //switch (initFiltersToApply[filterType]) {
+                    switch (filtersToApply[filterType]) {
+
+                        case "GreaterOrEqualThanExperienceYears":
+                            console.log("GreaterOrEqualThanExperienceYears")
+
+                            dataToRender =
+                                filterOffersByGreaterOrEqualThanExperienceYears(dataToRender, filtersToApply.experinceYears)
+                            break
+
+                        case "LessOrEqualThanExperienceYears":
+                            console.log("LessOrEqualThanExperienceYears")
+
+                            dataToRender =
+                                filterOffersByLessOrEqualThanExperienceYears(dataToRender, filtersToApply.experinceYears)
+                            break
+
+                        default:
+                            console.log("Error: this should be a new case for byExperienceYears")
+                    }
+                }
+            })
+
+        // Filter offers by type
+        //const dataFilterByOfferType = filterOffersByOfferType(dataOffer, "OfferMySelf")
+        //const dataFilterByOfferType2 = filterOffersByOfferType(dataOffer, "LookingForJob")
+
+
+        // Filter offers by average score
+        //const dataSortByAverageScore = sortOfersByAverageScore_descendingOrder(dataFilterByOfferType2)
+
+        // const dataFilterZero = dataSortByAverageScore.slice(0, itemsPerPage)
+        // const numerberPage = dataSortByAverageScore.length / itemsPerPage
+
+        const dataFilterZero = dataToRender.slice(0, itemsPerPage)
+        const numerberPage = dataToRender.length / itemsPerPage
 
         setPageCount(numerberPage)
-        setDataDevelopersList(dataSortByAverageScore)
+        //setDataDevelopersList(dataSortByAverageScore)
+        setDataDevelopersList(dataToRender)
         setItemPerPage(dataFilterZero)
         setDownloading(false);
     };
@@ -49,26 +329,26 @@ const OffersList = ({ itemsPerPage }) => {
                 <Spinner />
             ) : (
                 <div className="offersList-paginate-and-offers-list-container">
+
                     <div className="offersList-offers-container">
                         {itemPerPage.map((offer) => (
                             <div key={offer._id}>
                                 <CardOffer offer={offer} />
                             </div>
                         ))}
-                
+                    </div>
+
                     < ReactPaginate
                         className="offersList-paginate"
                         activeClassName="offersList-paginate-active-element"
                         breakLabel="..."
-                        nextLabel="siguiente >"
+                        nextLabel="next >"
                         onPageChange={handlePageClick}
-                        pageRangeDisplayed={10}
+                        pageRangeDisplayed={5}
                         pageCount={pageCount}
-                        previousLabel="< anterior"
+                        previousLabel="< previous"
                         renderOnZeroPageCount={null}
                     />
-                    
-                    </div>
                 </div>
             )}
         </div>
--- src/util/filters/offer.filter.js
+++ src/util/filters/offer.filter.js
@@ -15,6 +15,8 @@ export const sortOfersByAverageScore_descendingOrder = (offers) =>
 
 export const sortOffersByAverageScore_ascendingOrder = (offers) =>
     offers.sort((offerA, offerB) => {
+        console.log("offer A: ", offerA)
+        console.log("offer B: ", offerB)
         const offerA_score = offerA.ratings.reduce(getOfferTotalScore, 0);
         let offerA_scoreAverage = 0;
         if (offerA.ratings.length != 0) offerA_scoreAverage = offerA_score / offerA.ratings.length;
@@ -25,3 +27,26 @@ export const sortOffersByAverageScore_ascendingOrder = (offers) =>
 
         return offerA_scoreAverage - offerB_scoreAverage;
     });
+
+export const filterOffersByOfferType = (offers, offerType) =>
+    offers.filter(offer => offer.offerType === offerType)
+
+
+export const filterOffersByJobType = (offers, jobType) =>
+    offers.filter(offer => offer.jobType === jobType)
+
+export const filterOffersByOfferState = (offers, offerState) =>
+    offers.filter(offer => offer.offerState === offerState)
+
+
+export const filterOffersByGreaterOrEqualThanAnnualSalary = (offers, annualSalary) =>
+    offers.filter(offer => offer.annualSalary >= annualSalary)
+
+export const filterOffersByLessOrEqualThanAnnualSalary = (offers, annualSalary) =>
+    offers.filter(offer => offer.annualSalary <= annualSalary)
+
+export const filterOffersByGreaterOrEqualThanExperienceYears = (offers, experienceYears) =>
+    offers.filter(offer => offer.experienceYears >= experienceYears)
+
+export const filterOffersByLessOrEqualThanExperienceYears = (offers, experienceYears) =>
+    offers.filter(offer => offer.experienceYears <= experienceYears)
